b870a5571d33b621fe74c035670f0101d1524fcb
Omar de Santos <odesantos2@gmail.com>
2025-02-11 23:41:22 -0800

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
     watch: true,
     env: {
       NODE_ENV: 'development',
-      // Other environment variables
+      // Other environment variables as needed
     }
   }]
 };
